1827de07eb4249400f1d6371694588f7356f1e2b
Juliette Gondon <juliettegondon@gmail.com>
2020-10-31 13:09:17 -0400

    created db now I want a burger

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Kahuna Burger');
+INSERT INTO burgers (burger_name) VALUES ('Le Big Mac');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Royale with Cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder with Cheese', true);
+
