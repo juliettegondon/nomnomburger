cd395e6563c5ffb4f6df4ed75ef8a10737487cb1
Juliette Gondon <juliettegondon@gmail.com>
2020-11-13 14:38:29 -0500

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "juliette",
-  database: "burgers_db"
-});
+
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'juliette',
+    database: 'burgres_db'
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
